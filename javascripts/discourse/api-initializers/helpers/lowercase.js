7ddb00c4889d8638cdf3a0a30ba437e27149dce9
Bryce Huhtala <sketchius@gmail.com>
2024-03-30 17:47:19 -0500

    FIX: Address linting errors

--- javascripts/discourse/api-initializers/helpers/lowercase.js
+++ javascripts/discourse/api-initializers/helpers/lowercase.js
@@ -1,10 +1,10 @@
 import { registerUnbound } from "discourse-common/lib/helpers";
 
 export default {
-    name: 'lowercase-helper',
-    initialize() {
-        registerUnbound("lowercase", function (value) {
-            return value && typeof value === "string" ? value.toLowerCase() : "";
-          });
-    }
-  };
+  name: "lowercase-helper",
+  initialize() {
+    registerUnbound("lowercase", function (value) {
+      return value && typeof value === "string" ? value.toLowerCase() : "";
+    });
+  },
+};
--- javascripts/discourse/initializers/conditional-component-rendering.js
+++ javascripts/discourse/initializers/conditional-component-rendering.js
@@ -1,17 +1,18 @@
-
 import { apiInitializer } from "discourse/lib/api";
 import { defaultHomepage } from "discourse/lib/utilities";
 
 export default apiInitializer("1.8.0", (api) => {
-    api.onPageChange(() => {
-        const router = api.container.lookup('service:router');
+  api.onPageChange(() => {
+    const router = api.container.lookup("service:router");
 
-        if (router.currentRoute.name === `discovery.${defaultHomepage()}`) {
-          document.querySelectorAll('.d-header .floating-search-input-wrapper')[0].style.display = 'none';
-        } else {
-          document.querySelectorAll('.d-header .floating-search-input-wrapper')[0].style.display = 'flex';
-        }
-      });
+    if (router.currentRoute.name === `discovery.${defaultHomepage()}`) {
+      document.querySelectorAll(
+        ".d-header .floating-search-input-wrapper"
+      )[0].style.display = "none";
+    } else {
+      document.querySelectorAll(
+        ".d-header .floating-search-input-wrapper"
+      )[0].style.display = "flex";
+    }
+  });
 });
-
-
