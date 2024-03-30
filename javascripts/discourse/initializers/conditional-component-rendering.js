b5a5fb0603236edc15bcf1b2e799433e58bc8d2b
Bryce Huhtala <sketchius@gmail.com>
2024-03-30 16:54:52 -0500

    DEV: Install "Discourse Header Search" component with conditional display

---
+++ javascripts/discourse/initializers/conditional-component-rendering.js
@@ -0,0 +1,17 @@
+
+import { apiInitializer } from "discourse/lib/api";
+import { defaultHomepage } from "discourse/lib/utilities";
+
+export default apiInitializer("1.8.0", (api) => {
+    api.onPageChange(() => {
+        const router = api.container.lookup('service:router');
+
+        if (router.currentRoute.name === `discovery.${defaultHomepage()}`) {
+          document.querySelectorAll('.d-header .floating-search-input-wrapper')[0].style.display = 'none';
+        } else {
+          document.querySelectorAll('.d-header .floating-search-input-wrapper')[0].style.display = 'flex';
+        }
+      });
+});
+
+
