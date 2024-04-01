39e183087ec8b3af37df8077a866adae25a3a220
Bryce Huhtala <sketchius@gmail.com>
2024-04-01 18:08:04 -0500

    FIX: Address linting errors

--- javascripts/discourse/components/resource-card.js
+++ javascripts/discourse/components/resource-card.js
@@ -3,34 +3,34 @@ import Component from "@glimmer/component";
 export default class ResourceCard extends Component {
   get title() {
 
-    return settings[`${this.args.group}_resource_${this.args.id}_title`];
+    return settings[`${this.args.group}_resource_${this.args.cardId}_title`];
   }
 
   get description() {
-    return settings[`${this.args.group}_resource_${this.args.id}_description`];
+    return settings[`${this.args.group}_resource_${this.args.cardId}_description`];
   }
 
   get caption() {
-    return settings[`${this.args.group}_resource_${this.args.id}_caption`];
+    return settings[`${this.args.group}_resource_${this.args.cardId}_caption`];
   }
 
   get logo() {
-    return settings[`${this.args.group}_resource_${this.args.id}_logo`];
+    return settings[`${this.args.group}_resource_${this.args.cardId}_logo`];
   }
 
   get background() {
-    return settings[`${this.args.group}_resource_${this.args.id}_background`];
+    return settings[`${this.args.group}_resource_${this.args.cardId}_background`];
   }
 
   get invertStyle() {
-    return settings[`${this.args.group}_resource_${this.args.id}_invert`] ? "invert " : "";
+    return settings[`${this.args.group}_resource_${this.args.cardId}_invert`] ? "invert " : "";
   }
 
   get headingStyle() {
-    return settings[`${this.args.group}_resource_${this.args.id}_logo`] ? "large " : "";
+    return settings[`${this.args.group}_resource_${this.args.cardId}_logo`] ? "large " : "";
   }
 
   get bgcolor() {
-    return settings[`${this.args.group}_resource_${this.args.id}_bgcolor`];
+    return settings[`${this.args.group}_resource_${this.args.cardId}_bgcolor`];
   }
 }
