6e5f3bc18b0d47164a039d83ecee6d3e137eaf78
Bryce Huhtala <sketchius@gmail.com>
2024-04-03 13:37:09 -0500

    DEV: Create Category Stats ember component (with styling)

---
+++ javascripts/discourse/components/category-stats.js
@@ -0,0 +1,34 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { inject as service } from "@ember/service";
+import { withPluginApi } from 'discourse/lib/plugin-api';
+import { lookupCategoryByPath } from "../../utils/categoryLookup";
+
+export default class CategoryStats extends Component {
+  @service router;
+  @tracked category = null;
+
+  constructor() {
+    super(...arguments);
+    this.loadCategory();
+
+    withPluginApi('1.8', api => {
+      api.onPageChange(() => {
+        this.loadCategory();
+      });
+    });
+  }
+
+  async loadCategory() {
+    this.category = null;
+    const result = await lookupCategoryByPath(this.router.currentURL);
+    if (result.found) {
+      this.category = result.match;
+    }
+  }
+
+  get shouldRender() {
+    return this.category != null;
+  }
+
+}
